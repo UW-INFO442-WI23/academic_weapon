517260ad0495653310cd1af8adafd39beac200ba
Yeary <yyuan02@uw.edu>
2023-02-15 00:40:29 -0800

    about page css

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -6,60 +6,65 @@ function About() {
             <div className='coverImage'></div>
 
             <div className='mission'>
-                <p>Our Mission</p>
-                <p>Increase access to healthy and affordable produce in low-income, urban communities, focusing on our own local communities within King County in Washington State.</p>
-                <button>explore</button>
+                <p className='titleText'>Our Mission</p>
+                <p className='bodyText missionDes'>Increase access to healthy and affordable produce in low-income, urban communities, focusing on our own local communities within King County in Washington State.</p>
+                <button className='button-3'>explore</button>
             </div>
 
             <div className='impacts'>
                 <div className='impact1'>
-                    <div className='impact1img'></div>
-                    <div className='impact1name'>Impact 1</div>
-                    <p>Impacting ...</p>
+                    <div className='impact1img center'></div>
+                    <div className='subtitleText'>Impact 1</div>
+                    <p className='bodyText'>Impacting ...</p>
                 </div>
-                <div className='impacts'>
-                    <div className='impact2'>
-                        <div className='impact2img'></div>
-                        <div className='impact2name'>Impact 2</div>
-                        <p>Impacting ...</p>
-                    </div>
+
+                <div className='impact2'>
+                    <div className='impact2img center'></div>
+                    <div className='subtitleText'>Impact 2</div>
+                    <p className='bodyText'>Impacting ...</p>
                 </div>
-                <div className='impacts'>
-                    <div className='impact3'>
-                        <div className='impact3img'></div>
-                        <div className='impact3name'>Impact 3</div>
-                        <p>Impacting ...</p>
-                    </div>
+
+                <div className='impact3'>
+                    <div className='impact3img center'></div>
+                    <div className='subtitleText'>Impact 3</div>
+                    <p className='bodyText'>Impacting ...</p>
                 </div>
             </div>
 
             <div className='source'>
-                <div>Where Does the Produce Come From?</div>
-                <div>“Welcome to the farm outreach team! 
-                    Our goal is to visit both private and public farms in King County to understand the needs of local farmers, test the quality of their produce, and ensure food security for our community. 
-                    We will be helping these farmers post their produce on our website to connect them with consumers. We are excited to work with the local farming community and help support their efforts to provide fresh and high-quality food to our community.”
+                <p className='titleText'>Where Does the Produce Come From?</p>
+                <div className='sourceContainer'>
+                    <div className='bodyText item1'>“Welcome to the farm outreach team! 
+                        Our goal is to visit both private and public farms in King County to understand the needs of local farmers, test the quality of their produce, and ensure food security for our community. 
+                        <br></br><br></br>We will be helping these farmers post their produce on our website to connect them with consumers. We are excited to work with the local farming community and help support their efforts to provide fresh and high-quality food to our community.”
+                    </div>
+                    <div className='sourceImg'></div>
                 </div>
-                <div className='sourceImg'></div>
             </div>
 
-            <div className='howToOder'>
-                <div>How To Order</div>
+            <p className='titleText'>How To Order</p>
+            <div className='howToOrder'>
                 <div className='search'>
                     <div className='searchImg'></div>
-                    <div className='orderText'>1. Search for produce</div>
+                    <div className='bodyText'>1. Search for produce</div>
                 </div>
                 <div className='compare'>
                     <div className='compareImg'></div>
-                    <div className='orderText'>2. Compare & Select</div>
+                    <div className='bodyText'>2. Compare & Select</div>
                 </div>
                 <div className='select'>
                     <div className='selectImg'></div>
-                    <div className='orderText'>3. Select  times & location</div>
+                    <div className='bodyText'>3. Select  times & location</div>
                 </div>
                 <div className='send'>
                     <div className='sendImg'></div>
-                    <div className='orderText'>4. Send order</div>
+                    <div className='bodyText'>4. Send order</div>
                 </div>
+            </div>
+
+            <div>
+                <p className='titleText'>Related Resources</p>
+                <p><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br></p>
             </div>
         </div>
     )
